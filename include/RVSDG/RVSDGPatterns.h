b88bf1fa803cf2b2a087607d44df083938b9ed5b
Halvor Linder <halvor_lh@hotmail.no>
2025-03-14 10:38:09 +0100

    wip: add patterns for casts and globals

---
+++ include/RVSDG/RVSDGPatterns.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <memory>
+#include <mlir/Pattern/Pattern.h>
+
+#include "RVSDG/RVSDGDialect.h"
+#include "RVSDG/RVSDGOps.h"
+
+#include <mlir/Dialect/Arith/IR/Arith.h>
+#include <mlir/Dialect/Func/IR/FuncOps.h>
+#include <mlir/Dialect/LLVMIR/LLVMDialect.h>
+#include <mlir/Dialect/MemRef/IR/MemRef.h>
+#include <mlir/Dialect/SCF/IR/SCF.h>
+#include <polygeist/Dialect.h>
+
+#define GEN_PASS_DECL
+#include "RVSDG/Patterns.h.inc"
+
+// Declare pass creation functions
+namespace mlir
+{
+namespace rvsdg
+{
+} // namespace rvsdg
+} // namespace mlir
+
+#define GEN_PASS_REGISTRATION
+#include "RVSDG/Patterns.h.inc"
--- lib/RVSDG/RVSDGImportPolygeistPass.cpp
+++ lib/RVSDG/RVSDGImportPolygeistPass.cpp
@@ -1,3 +1,9 @@
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
+#include "mlir/IR/PatternMatch.h"
+#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
+#include "RVSDG/RVSDGDialect.h"
 #include "RVSDG/RVSDGPasses.h"
 #include <mlir/Pass/PassManager.h>
 
@@ -5,6 +11,47 @@ namespace mlir::rvsdg::importPolygeistPass
 {
 #define GEN_PASS_DEF_RVSDG_IMPORTPOLYGEISTPASS
 #include "RVSDG/Passes.h.inc"
+#include "RVSDG/Patterns.h.inc"
+
+// Helper function to convert LLVM.mlir.global to RVSDG deltanode
+static Value
+convertLLVMGlobalToRVSDGDelta(
+    PatternRewriter & rewriter,
+    Operation * op,
+    Attribute symNameAttr,
+    Attribute valueAttr)
+{
+  auto loc = op->getLoc();
+  auto globalOp = cast<LLVM::GlobalOp>(op);
+
+  auto deltaNode = rewriter.create<mlir::rvsdg::DeltaNode>(loc);
+  Region & deltaRegion = deltaNode.getBody();
+  auto * block = rewriter.createBlock(&deltaRegion);
+
+  // Position the insertion point at the end of the block
+  rewriter.setInsertionPointToEnd(block);
+
+  // Get the global value
+  Value globalValue;
+  if (valueAttr && !valueAttr.isa<UnitAttr>())
+  {
+    // If the global has an initializer, use it
+    globalValue =
+        rewriter.create<LLVM::ConstantOp>(loc, globalOp.getType(), valueAttr);
+  }
+  else
+  {
+    // Otherwise, create a zero value
+    globalValue = rewriter.create<LLVM::UndefOp>(loc, globalOp.getType());
+  }
+
+  // Create the delta result operation
+  rewriter.create<mlir::rvsdg::DeltaResult>(loc, globalValue);
+
+  // Return the deltanode
+  return deltaNode.getOutput();
+}
+
 }
 
 struct ImportPolygeistPass
@@ -19,30 +66,35 @@ struct ImportPolygeistPass
   bool
   canScheduleOn(mlir::RegisteredOperationName opName) const override
   {
-    // return opName.isAnyOf("arith.addf");
+    return true;
   }
 
   void
   runOnOperation() override
   {
-    mlir::Operation * op = getOperation();
-    //     mlir::OpPassManager pm;
-    //     pm.addPass(mlir::createConvertPolygeistToRVSDGPass());
-    //     if (failed(runPipeline(pm, op)))
-    //     {
-    //         return signalPassFailure();
-    // }
+    mlir::RewritePatternSet patterns(&getContext());
 
-    for (auto & region : op->getRegions())
+    // Add the patterns generated from the TableGen definitions
+    mlir::rvsdg::importPolygeistPass::populateWithGenerated(patterns);
+
+    // Apply the patterns
+    if (mlir::failed(mlir::applyPatternsAndFoldGreedily(getOperation(), std::move(patterns))))
     {
-      for (auto & nested_op : region.getOps())
-      {
-        // std::cout << nested_op.getName().getStringRef().data() << std::endl;
-      }
+      signalPassFailure();
     }
   }
 
 private:
+  bool nestedPMInitialized = false;
+  mlir::OpPassManager nestedPM;
+
+  inline mlir::OpPassManager
+  initNestedPassManager()
+  {
+    mlir::OpPassManager nestedPM;
+    nestedPM.addPass(createRVSDG_ImportPolygeistPass());
+    return nestedPM;
+  }
 };
 
 std::unique_ptr<::mlir::Pass>
