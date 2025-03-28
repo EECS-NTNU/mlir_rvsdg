eba9cbc094c7ae530c1ba2d4a3a09cb438c9d65a
Halvor Linder <halvor_lh@hotmail.no>
2025-03-28 10:55:38 +0100

    comment out and remove unused stuff

--- lib/RVSDG/RVSDGImportPolygeistPass.cpp
+++ lib/RVSDG/RVSDGImportPolygeistPass.cpp
@@ -12,46 +12,6 @@ namespace mlir::rvsdg::importPolygeistPass
 #define GEN_PASS_DEF_RVSDG_IMPORTPOLYGEISTPASS
 #include "RVSDG/Passes.h.inc"
 #include "RVSDG/Patterns.h.inc"
-
-// Helper function to convert LLVM.mlir.global to RVSDG deltanode
-static Value
-convertLLVMGlobalToRVSDGDelta(
-    PatternRewriter & rewriter,
-    Operation * op,
-    Attribute symNameAttr,
-    Attribute valueAttr)
-{
-  auto loc = op->getLoc();
-  auto globalOp = cast<LLVM::GlobalOp>(op);
-
-  auto deltaNode = rewriter.create<mlir::rvsdg::DeltaNode>(loc);
-  Region & deltaRegion = deltaNode.getBody();
-  auto * block = rewriter.createBlock(&deltaRegion);
-
-  // Position the insertion point at the end of the block
-  rewriter.setInsertionPointToEnd(block);
-
-  // Get the global value
-  Value globalValue;
-  if (valueAttr && !valueAttr.isa<UnitAttr>())
-  {
-    // If the global has an initializer, use it
-    globalValue =
-        rewriter.create<LLVM::ConstantOp>(loc, globalOp.getType(), valueAttr);
-  }
-  else
-  {
-    // Otherwise, create a zero value
-    globalValue = rewriter.create<LLVM::UndefOp>(loc, globalOp.getType());
-  }
-
-  // Create the delta result operation
-  rewriter.create<mlir::rvsdg::DeltaResult>(loc, globalValue);
-
-  // Return the deltanode
-  return deltaNode.getOutput();
-}
-
 }
 
 struct ImportPolygeistPass
@@ -72,16 +32,8 @@ struct ImportPolygeistPass
   void
   runOnOperation() override
   {
-    mlir::RewritePatternSet patterns(&getContext());
-
-    // Add the patterns generated from the TableGen definitions
-    mlir::rvsdg::importPolygeistPass::populateWithGenerated(patterns);
-
-    // Apply the patterns
-    if (mlir::failed(mlir::applyPatternsAndFoldGreedily(getOperation(), std::move(patterns))))
-    {
-      signalPassFailure();
-    }
+    printf("Running ImportPolygeistPass\n");
+    
   }
 
 private:
